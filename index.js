a14ca63afd6c162f5954158dad0b8884a66bae73
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-02-25 21:12:18 +0500

    Updated File

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const elForm = document.querySelector('.form');
 const elInput = document.querySelector('#input');
 const elStatus = document.querySelector('.status');
+const elPopup = document.querySelector('.popup');
+const elBtnModal = document.querySelector('.btn-modal');
 
 function validateInput() {
     const inputValue = elInput.value.trim();
@@ -10,8 +12,18 @@ function validateInput() {
         elStatus.className = 'text-[red] font-bold text-[14px]';
         elInput.style.border = '2px solid red';
         elInput.className = 'incorrect border-2 border-[#64748b] py-[10px] px-[20px] text-[1.2rem] mt-[.5rem] mb-[1rem] rounded-md bg-[#fecaca]';
+    } else {
+        elPopup.classList.remove('invisible');
+        document.querySelector('.main-container').classList.add('blur-[30px]', 'duration-200');
     }
 }
+
+elBtnModal.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    elPopup.classList.add('invisible');
+    document.querySelector('.main-container').classList.remove('blur-[30px]', 'duration-200');
+})
 
 elForm.addEventListener('submit', (e) => {
     e.preventDefault();
